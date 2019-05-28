9448e407dbba17e38c6e5a990edddd39d211275e
Munoon <munoongg@gmail.com>
2019-05-28 21:43:43 +0300

    Added testing code

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -20,9 +20,11 @@ public class UserMealsUtil {
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,13,0), "Обед", 500),
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,20,0), "Ужин", 510)
         );
-        getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
-//        .toLocalDate();
-//        .toLocalTime();
+        List<UserMealWithExceed> test1 = getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
+        List<UserMealWithExceed> test2 = getFilteredWithExceeded2(mealList, LocalTime.of(13, 0), LocalTime.of(18, 0), 2000);
+
+        System.out.println("Test 1 - " + (test1.size() == 2 ? "correct" : "wrong"));
+        System.out.println("Test 2 - " + (test2.size() == 2 ? "correct" : "wrong"));
     }
 
     public static List<UserMealWithExceed> getFilteredWithExceeded(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
@@ -34,5 +36,10 @@ public class UserMealsUtil {
             }
         });
         return resultUsers;
+    }
+
+    public static List<UserMealWithExceed> getFilteredWithExceeded2(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
+        // TODO implement this function with stream api
+        return new ArrayList<>();
     }
 }

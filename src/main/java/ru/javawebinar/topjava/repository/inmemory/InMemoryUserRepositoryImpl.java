d99fda59adec412730bdc8ce287e8c2aa43c2cde
Munoon <munoongg@gmail.com>
2019-06-14 11:36:57 +0300

    Fixed InMemoryUserRepository

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepositoryImpl.java
@@ -53,6 +53,10 @@ public class InMemoryUserRepositoryImpl implements UserRepository {
     @Override
     public User getByEmail(String email) {
         log.info("getByEmail {}", email);
-        return repository.get(email);
+        return getAll()
+                .stream()
+                .filter(user -> user.getEmail() == email)
+                .findFirst()
+                .get();
     }
 }

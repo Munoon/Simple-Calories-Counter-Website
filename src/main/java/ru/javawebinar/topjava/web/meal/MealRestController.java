4e974c4e7f3d277bb20fdd34c319da919d1ba8e3
Munoon <munoongg@gmail.com>
2019-06-16 19:18:34 +0300

    Implemented MealRestController

--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -1,8 +1,46 @@
 package ru.javawebinar.topjava.web.meal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
+import ru.javawebinar.topjava.util.ValidationUtil;
+import ru.javawebinar.topjava.web.SecurityUtil;
 
+import java.util.List;
+
+@Controller
 public class MealRestController {
+    protected final Logger log = LoggerFactory.getLogger(getClass());
+
+    @Autowired
     private MealService service;
+
+    public Meal create(Meal meal) {
+        ValidationUtil.checkNew(meal);
+        log.info("Creating {}", meal);
+        return service.create(meal);
+    }
+
+    public void delete(int id) {
+        log.info("Delete meal {}", id);
+        service.delete(id, SecurityUtil.authUserId());
+    }
+
+    public Meal get(int id) {
+        log.info("Get meal {}", id);
+        return service.get(id, SecurityUtil.authUserId());
+    }
+
+    public void update(Meal meal) {
+        log.info("Updated {}", meal);
+        service.update(meal, SecurityUtil.authUserId());
+    }
+
+    public List<Meal> getAll() {
+        return service.getAll(SecurityUtil.authUserId());
+    }
 
 }

c1325081f999137d229cccfb0f192eaf7c923158
Munoon <munoongg@gmail.com>
2019-06-17 11:59:28 +0300

    Made DateTimeUtil#isBetween in one function

--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -3,18 +3,14 @@ package ru.javawebinar.topjava.web.meal;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cglib.core.Local;
 import org.springframework.stereotype.Controller;
 import ru.javawebinar.topjava.model.Meal;
-import ru.javawebinar.topjava.model.MealTo;
 import ru.javawebinar.topjava.service.MealService;
 import ru.javawebinar.topjava.util.DateTimeUtil;
-import ru.javawebinar.topjava.util.MealsUtil;
 import ru.javawebinar.topjava.util.ValidationUtil;
 import ru.javawebinar.topjava.web.SecurityUtil;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -54,10 +50,10 @@ public class MealRestController {
     public List<Meal> getAllWithFilter(LocalDate startDate, LocalDate endDate, LocalTime startTime, LocalTime endTime) {
         return service.getAll(SecurityUtil.authUserId())
                 .stream()
-                .filter(meal -> DateTimeUtil.isBetweenDate(meal.getDate(),
+                .filter(meal -> DateTimeUtil.isBetween(meal.getDate(),
                         startDate == null ? LocalDate.MIN : startDate,
                         endDate == null ? LocalDate.MAX : endDate))
-                .filter(meal -> DateTimeUtil.isBetweenTime(meal.getTime(),
+                .filter(meal -> DateTimeUtil.isBetween(meal.getTime(),
                         startTime == null ? LocalTime.MIN : startTime,
                         endTime == null ? LocalTime.MAX : endTime))
                 .collect(Collectors.toList());

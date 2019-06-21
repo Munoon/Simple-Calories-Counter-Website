2eecf500d71a4ecbbd6c5b3195889d295acf5d86
Munoon <munoongg@gmail.com>
2019-06-21 08:57:04 +0300

    Added MealServiceTest

---
+++ src/test/java/ru/javawebinar/topjava/MealTestData.java
@@ -0,0 +1,40 @@
+package ru.javawebinar.topjava;
+
+import ru.javawebinar.topjava.model.Meal;
+
+import java.lang.reflect.Array;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MealTestData {
+    public static final int FIRST_MEAL_ID = 100000;
+    public static final Meal FIRST_MEAL = new Meal(100000, LocalDateTime.of(2019, 6, 20, 12, 0, 0), "Завтрак", 500);
+    public static final List<Meal> FIRST_USER_MEALS = Arrays.asList(
+            new Meal(100000, LocalDateTime.of(2019, 6, 20, 12, 0, 0), "Завтрак", 500),
+            new Meal(100001, LocalDateTime.of(2019, 6, 20, 14, 0, 0), "Обед", 500),
+            new Meal(100002, LocalDateTime.of(2019, 6, 20, 18, 0, 0), "Ужин", 700),
+            new Meal(100003, LocalDateTime.of(2019, 6, 21, 12, 0, 0), "Завтрак", 500),
+            new Meal(100004, LocalDateTime.of(2019, 6, 21, 14, 0, 0), "Обед", 500),
+            new Meal(100005, LocalDateTime.of(2019, 6, 21, 18, 0, 0), "Ужин", 700)
+    );
+
+    public static void assertMatchIgnoreDateTime(Meal actual, Meal expected) {
+        assertThat(actual).isEqualToIgnoringGivenFields(expected, "dateTime");
+    }
+
+    public static void assertMatch(Meal actual, Meal expected) {
+        assertThat(actual).isEqualTo(expected);
+    }
+
+
+    public static void assertMatch(Iterable<Meal> actual, Iterable<Meal> expected) {
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    public static void assertMatchIgnoreDateTime(Iterable<Meal> actual, Iterable<Meal> expected) {
+        assertThat(actual).usingElementComparatorIgnoringFields("dateTime").isEqualTo(expected);
+    }
+}
---
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -0,0 +1,100 @@
+package ru.javawebinar.topjava.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.bridge.SLF4JBridgeHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cglib.core.Local;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.test.context.junit4.SpringRunner;
+import ru.javawebinar.topjava.MealTestData;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.util.DateTimeUtil;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static ru.javawebinar.topjava.MealTestData.*;
+import static ru.javawebinar.topjava.UserTestData.USER_ID;
+
+@ContextConfiguration({
+        "classpath:spring/spring-app.xml",
+        "classpath:spring/spring-db.xml"
+})
+@RunWith(SpringRunner.class)
+@Sql(scripts = "classpath:db/initDB.sql", config = @SqlConfig(encoding = "UTF-8"))
+@Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
+public class MealServiceTest {
+
+    static {
+        SLF4JBridgeHandler.install();
+    }
+
+    @Autowired
+    MealService service;
+
+    @Test
+    public void get() {
+        Meal meal = service.get(FIRST_MEAL_ID, USER_ID);
+        MealTestData.assertMatch(meal, FIRST_MEAL);
+    }
+
+    @Test
+    public void delete() {
+        List<Meal> all = service.getAll(USER_ID);
+        service.delete(FIRST_MEAL_ID, USER_ID);
+        List<Meal> collect = all.stream().filter(meal -> meal.getId() != FIRST_MEAL_ID).collect(Collectors.toList());
+        assertMatch(service.getAll(USER_ID), collect);
+    }
+
+    @Test
+    public void getBetweenDates() {
+        LocalDate date = LocalDate.of(2019, 6, 20);
+        List<Meal> expected = FIRST_USER_MEALS
+                .stream()
+                .filter(meal -> meal.getDate().isEqual(date))
+                .collect(Collectors.toList());
+        assertMatch(service.getBetweenDates(date, date, USER_ID), expected);
+    }
+
+    @Test
+    public void getBetweenDateTimes() {
+        LocalDateTime start = LocalDateTime.of(2019, 6, 20, 12, 0, 0);
+        LocalDateTime end = LocalDateTime.of(2019, 6, 21, 14, 0, 0);
+        List<Meal> expected = FIRST_USER_MEALS
+                .stream()
+                .filter(meal -> meal.getDateTime().isBefore(end) && meal.getDateTime().isAfter(start))
+                .collect(Collectors.toList());
+        assertMatch(service.getBetweenDateTimes(start, end, USER_ID), expected);
+    }
+
+    @Test
+    public void getAll() {
+        assertMatch(service.getAll(USER_ID), FIRST_USER_MEALS);
+    }
+
+    @Test
+    public void update() {
+        Meal updated = new Meal(FIRST_MEAL);
+        updated.setDescription("Test Description");
+        updated.setCalories(999);
+        service.update(updated, USER_ID);
+        MealTestData.assertMatchIgnoreDateTime(service.get(FIRST_MEAL_ID, USER_ID), updated);
+    }
+
+    @Test
+    public void create() {
+        List<Meal> list = new ArrayList<>(service.getAll(USER_ID));
+        Meal newMeal = new Meal(LocalDateTime.of(2019, 6, 22, 14, 0, 0), "Тест Обед", 200);
+        Meal created = service.create(newMeal, USER_ID);
+        newMeal.setId(created.getId());
+        list.add(newMeal);
+        assertMatch(service.getAll(USER_ID), list);
+    }
+}

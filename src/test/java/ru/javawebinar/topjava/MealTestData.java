af669404f4015f05e2fb23baf27e4babec0a0264
Munoon <munoongg@gmail.com>
2019-06-22 11:25:51 +0300

    Make assertThat method better

--- src/test/java/ru/javawebinar/topjava/MealTestData.java
+++ src/test/java/ru/javawebinar/topjava/MealTestData.java
@@ -4,6 +4,7 @@ import ru.javawebinar.topjava.model.Meal;
 
 import java.time.LocalDateTime;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -21,14 +22,18 @@ public class MealTestData {
     );
 
     public static void assertMatch(Meal actual, Meal expected) {
-        assertThat(actual).isEqualTo(expected);
+        assertThat(actual).isEqualToComparingFieldByFieldRecursively(expected);
     }
 
     public static void assertMatch(Iterable<Meal> actual, Iterable<Meal> expected) {
-        assertThat(actual).isEqualTo(expected);
+        Iterator<Meal> actualIterator = actual.iterator();
+        Iterator<Meal> expectedIterator = expected.iterator();
+        while (actualIterator.hasNext()) {
+            assertMatch(actualIterator.next(), expectedIterator.next());
+        }
     }
 
     public static void assertMatch(Iterable<Meal> actual, Meal... expected) {
-        assertThat(actual).isEqualTo(Arrays.asList(expected));
+        assertMatch(actual, Arrays.asList(expected));
     }
 }

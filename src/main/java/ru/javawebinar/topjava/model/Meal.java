009d596166967d9ca19a996455b3ffbf8b5ac2bc
Munoon <munoongg@gmail.com>
2019-06-28 18:20:30 +0300

    Updated Meal model

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -1,19 +1,21 @@
 package ru.javawebinar.topjava.model;
 
-import ru.javawebinar.topjava.util.LocalDateTimeConvertor;
+import org.hibernate.validator.constraints.Range;
+import org.springframework.lang.Nullable;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 @Entity
-@Table(name = "meals", uniqueConstraints = {
-        @UniqueConstraint(columnNames = "user_id", name = "meals_unique_user_datetime_idx"),
-        @UniqueConstraint(columnNames = "date_time", name = "meals_unique_user_datetime_idx")
-})
+@Table(name = "meals", uniqueConstraints =
+        @UniqueConstraint(columnNames = {"user_id", "date_time"}, name = "meals_unique_user_datetime_idx")
+)
 @NamedQueries({
         @NamedQuery(name = Meal.GET, query = "SELECT m FROM Meal m WHERE m.id=:id AND m.user.id=:userId"),
         @NamedQuery(name = Meal.GET_ALL, query = "SELECT m FROM Meal m WHERE m.user.id=:userId ORDER BY m.dateTime DESC"),
@@ -32,21 +34,22 @@ public class Meal extends AbstractBaseEntity {
     public static final String DELETE = "Meal.delete";
     public static final String UPDATE = "Meal.update";
 
-    @Column(name = "date_time", nullable = false, unique = true)
-    @Convert(converter = LocalDateTimeConvertor.class)
+    @Column(name = "date_time", nullable = false)
     @NotNull
     private LocalDateTime dateTime;
 
     @Column(name = "description", nullable = false)
+    @Size(min = 2, max = 120)
     @NotBlank
     private String description;
 
     @Column(name = "calories", nullable = false)
+    @Range(min = 10, max = 5000)
     private int calories;
 
-    @CollectionTable(name = "users", joinColumns = @JoinColumn(name = "id"))
-    @JoinColumn(name = "user_id")
-    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id", foreignKey = @ForeignKey(name = "global_seq", foreignKeyDefinition = "START WITH 100000"))
+    @ManyToOne(fetch = FetchType.LAZY)
+    @Nullable
     private User user;
 
     public Meal() {

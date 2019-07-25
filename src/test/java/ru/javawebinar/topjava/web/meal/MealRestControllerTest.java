a1988d9d9fcc3eed7c393e5f73fcf1f0dc91bec0
Munoon <munoongg@gmail.com>
2019-07-25 12:18:24 +0300

    Implement MealRestController

---
+++ src/test/java/ru/javawebinar/topjava/web/meal/MealRestControllerTest.java
@@ -0,0 +1,97 @@
+package ru.javawebinar.topjava.web.meal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.to.MealTo;
+import ru.javawebinar.topjava.util.MealsUtil;
+import ru.javawebinar.topjava.web.AbstractControllerTest;
+import ru.javawebinar.topjava.web.json.JsonUtil;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static ru.javawebinar.topjava.MealTestData.*;
+import static ru.javawebinar.topjava.TestUtil.readFromJson;
+import static ru.javawebinar.topjava.UserTestData.USER_ID;
+
+class MealRestControllerTest extends AbstractControllerTest {
+    private static final String REST_URL = MealRestController.REST_URL + '/';
+
+    @Test
+    void testGet() throws Exception {
+        mockMvc.perform(get(REST_URL + MEAL1_ID))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(contentJson(MEAL1));
+    }
+
+    @Test
+    void testDelete() throws Exception {
+        mockMvc.perform(delete(REST_URL + MEAL1_ID))
+                .andDo(print())
+                .andExpect(status().isNoContent());
+        assertMatch(mealService.getAll(USER_ID), MEAL6, MEAL5, MEAL4, MEAL3, MEAL2);
+    }
+
+    @Test
+    void getAll() throws Exception {
+        List<MealTo> expected = MealsUtil.getWithExcess(Arrays.asList(MEAL6, MEAL5, MEAL4, MEAL3, MEAL2, MEAL1), MealsUtil.DEFAULT_CALORIES_PER_DAY);
+        mockMvc.perform(get(REST_URL))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(contentJson(expected));
+    }
+
+    @Test
+    void testCreate() throws Exception {
+        Meal expected = new Meal(null, LocalDateTime.now(), "New Meal", MealsUtil.DEFAULT_CALORIES_PER_DAY);
+        ResultActions actions = mockMvc.perform(post(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(expected)))
+                .andExpect(status().isCreated());
+
+        Meal returned = readFromJson(actions, Meal.class);
+        expected.setId(returned.getId());
+
+        assertMatch(returned, expected);
+        assertMatch(mealService.getAll(USER_ID), expected, MEAL6, MEAL5, MEAL4, MEAL3, MEAL2, MEAL1);
+    }
+
+    @Test
+    void testUpdate() throws Exception {
+        Meal updated = new Meal(MEAL1);
+        updated.setDescription("Updated Description");
+        updated.setCalories(700);
+
+        mockMvc.perform(put(REST_URL + MEAL1_ID)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(updated)))
+                .andExpect(status().isNoContent());
+
+        assertMatch(mealService.get(MEAL1_ID, USER_ID), updated);
+    }
+
+    @Test
+    void getBetween() throws Exception {
+        List<MealTo> expected = MealsUtil.getWithExcess(Arrays.asList(MEAL6, MEAL5, MEAL4), MealsUtil.DEFAULT_CALORIES_PER_DAY);
+
+        LocalDateTime startDateTime = LocalDateTime.of(2015, 5, 31, 10, 0);
+        LocalDateTime endDateTime = LocalDateTime.of(2015, 5, 31, 20, 0);
+        String start = DateTimeFormatter.ISO_LOCAL_DATE_TIME.format(startDateTime);
+        String end = DateTimeFormatter.ISO_LOCAL_DATE_TIME.format(endDateTime);
+
+        mockMvc.perform(get(REST_URL + String.format("filter?start=%s&end=%s", start, end)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(contentJson(expected));
+    }
+}
